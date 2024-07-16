ea06190cc0e8cd7f380167b024476105e1e51366
unknown <C:\Users\User\AppData\Roaming\The Bat!>
2024-07-16 11:47:26 +0500

    test deploy

--- setup.py
+++ setup.py
@@ -1,21 +1,25 @@
 from setuptools import setup
 
-APP = ['cv-mac.py']  # Замените 'main.py' на имя вашего скрипта
-DATA_FILES = []
+APP = ['your_script.py']  # Замените 'your_script.py' на имя вашего основного скрипта
+
 OPTIONS = {
     'argv_emulation': True,
-    'packages': ['cv2', 'requests'],
+    'packages': ['cv2', 'dlib', 'requests', 'ftplib', 'platform', 'os', 'socket', 'time'],
     'plist': {
         'CFBundleName': 'YourAppName',
-        'CFBundleDisplayName': 'YourAppName',
-        'CFBundleIdentifier': 'com.yourname.yourappname',
-        'CFBundleVersion': '0.1.0',
+        'CFBundleDisplayName': 'Your App Name',
+        'CFBundleGetInfoString': "Your App Description",
+        'CFBundleIdentifier': "com.yourcompany.yourapp",
+        'CFBundleVersion': "0.1.0",
+        'CFBundleShortVersionString': "0.1.0",
+        'CFBundleExecutable': "YourAppName",
+        'LSUIElement': True,  # Если ваше приложение не показывает окно
     },
+    'iconfile': 'icon.icns',  # Укажите путь к вашему файлу иконки, если требуется
 }
 
 setup(
     app=APP,
-    data_files=DATA_FILES,
     options={'py2app': OPTIONS},
     setup_requires=['py2app'],
 )
